4f100a412d0bbfb64a3d6842e6146b68022880b3
momikus <yavuzkirez@gmail.com>
2014-03-08 15:56:25 +0200

    update ekranında ay ve gün sorunu görünmesi durumu çözüldü

--- client/helpers/birimOlay.js
+++ client/helpers/birimOlay.js
@@ -1,15 +1,10 @@
-// Template.birimOlayInput.rendered = function() {
-// 	Meteor.setTimeout(function() {
-// 	$('.birimOlay').each(function() {
-		
-// 		var month = parseInt($(this).find('select.month').val());
-// 		console.log(month);
-// 		//month = 5;
-// 		$(this).find('select.month').find('option[value='+month+']').attr('selected','selected');
-// 		// var day = parseInt($(this).find('select.day').val());
-// 		// $("#gate").val('Gateway 2');
-// 	});
-// 	}, 1000)
-
-// }
+Template.birimOlayInput.rendered = function() {
+	// update ekranına varolan ay ve günleri selected almak
+	$('.birimOlay').each(function() {
+		var month = parseInt($(this).find('select[name^="monthlist"]').attr("value"));
+		$(this).find('select[name^="monthlist"] option[value="'+month+'"]').attr('selected','selected');
+		var day = parseInt($(this).find('select[name^="daylist"]').attr("value"));
+		$(this).find('select[name^="daylist"] option[value="'+day+'"]').attr('selected','selected');
+	});
+}
 
