30d3d603a1b209a84004f751ec026a2a038b7cd1
momikus <yavuzkirez@gmail.com>
2014-03-08 20:35:12 +0200

    no change actually

--- client/helpers/timeline.js
+++ client/helpers/timeline.js
@@ -25,6 +25,7 @@ Handlebars.registerHelper('ayIsmi', function(month) {
 		var result = "Aralık";
 	return result;
 });
+   
 Template.timeline.rendered = function () {
 	$('p').on('paste', function () {
 		$('p').delay(50).queue(function (next) {
