16f7c3802733a3222770500eed76b4a10e9984f6
ccan <cagdascan@gmail.com>
2014-03-19 14:00:41 +0200

    subs

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -25,61 +25,21 @@ Router.map(function () {
 			'header': {to: 'header'},
 		},
 		waitOn: function () {	
-
-			// Deps.autorun(function () {
-			// 	Meteor.subscribe('timelineMain', 'main', function () {
-			// 		var tid = Timeline.findOne({'status': 'published'}, {limit: 1, sort: {created: -1}}).tid;
-			// 		Session.set('singleTimeline', tid);
-			// 	});
-			// });
-			
-			// Carousel için subscribe - Deprecated
-			// Deps.autorun(function () {
-			// 	var skip = Session.get('skip');
-			// 	var admin;
-			// 	if (Session.equals('varoAdmin', true))
-			// 		admin = true;
-			// 	else
-			// 		admin = false;
-			// 	Meteor.subscribe('timelineCarousel', skip, admin, function () {
-			// 		Meteor.setTimeout(function () {
-			// 			Session.set('timelineCarouselLoaded', true);
-			// 		}, 500);
-			// 	});
-			// });
-
-			Deps.autorun(function () {
-				var admin;
-				if (Session.equals('varoAdmin', true))
-					admin = true;
-				else
-					admin = false;
-				Meteor.subscribe('timelineAll', admin, function () {
-					Session.set('timelineAllLoaded', true);
-				});
-			});
-
+			var admin;
+			if (Session.equals('varoAdmin', true))
+				admin = true;
+			else
+				admin = false;
+			Meteor.subscribe('timelineAll', admin);
 		},
-
 		data: {
-			// // skip last timeline in carousel
-			// timelineCarousel: function () {
-			// 	if (!Session.equals('skip', 0))
-			// 		return Timeline.find({}, {skip: 1, limit: 4, sort: {created: -1}});
-			// 	else
-			// 		return Timeline.find({}, {limit: 4, sort: {created: -1}});
-			// },
-
-			// // get the last timeline as main 
-			// timelineMain: function () {
-			// 	return Timeline.findOne({}, {limit: 1, sort: {created: -1}});
-			// }
 			timelineAll: function () {
 				return Timeline.find({}, {limit: 50, sort: {created: -1}});
 			},
 		},
 	});
 
+	// create new taym
 	this.route('add', {
 		path: '/taym-olustur',
 		template: 'entryInput',
@@ -116,6 +76,7 @@ Router.map(function () {
 		}
 	});
 
+	// edit current taym
 	this.route('edit', {
 		path: '/taym-duzenle',
 		template: 'entryInput',
@@ -157,37 +118,15 @@ Router.map(function () {
 		},
 
 		waitOn: function () {
-			Deps.autorun(function () {
-				var id = Session.get('singleTimeline');
-				if (Session.equals('varoAdmin', true))
-					admin = true;
-				else
-					admin = false;
-				Meteor.subscribe('timelineMain', 'single', id, 'admin');
-			});
-
-			Deps.autorun(function () {
-				var skip = Session.get('skip');
-				if (Session.equals('varoAdmin', true))
-					admin = true;
-				else
-					admin = false;
-				Meteor.subscribe('timelineCarousel', skip, admin, function () {
-					Meteor.setTimeout(function () {
-						Session.set('timelineCarouselLoaded', true);
-					}, 500);
-				});
-			});
+			var id = Session.get('singleTimeline');
+			if (Session.equals('varoAdmin', true))
+				admin = true;
+			else
+				admin = false;
+			Meteor.subscribe('timelineMain', 'single', id, admin);
 		},
 
 		data: {
-			
-			// skip last timeline in carousel
-			timelineCarousel: function () {
-				return Timeline.find({
-					'tid': {$ne: Session.get('singleTimeline')}
-				}, {limit: 4, sort: {created: -1}});
-			},
 
 			// set the main timeline according to url 
 			timelineMain: function () {
--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 Meteor.publish('timelineMain', function (route, id, admin) {
 	if (route === 'main')
-		return Timeline.find({'status': 'published'}, {limit: 1, sort: {created: -1}});
+		return Timeline.find({'status': 'published'},
+			{limit: 1, sort: {created: -1}});
 	else if (route === 'single') {
 		if (admin === true)
 			return Timeline.find({'tid': id}, {limit: 1});
@@ -9,17 +10,10 @@ Meteor.publish('timelineMain', function (route, id, admin) {
 	}
 });
 
-// Meteor.publish('timelineCarousel', function (skip, admin) {
-// 	console.log('admin ' + admin);
-// 	if (admin === true)
-// 		return Timeline.find({}, {limit: 5, skip: skip, sort: {created: -1}});
-// 	else
-// 		return Timeline.find({'status': 'published'}, {limit: 5, skip: skip, sort: {created: -1}});
-// });
-
 Meteor.publish('timelineAll', function (admin) {
 	if (admin === true)
 		return Timeline.find({}, {limit: 50, sort: {created: -1}});
 	else
-		return Timeline.find({'status': 'published'}, {limit: 50, sort: {created: -1}});
+		return Timeline.find({'status': 'published'},
+			{limit: 50, sort: {created: -1}});
 });
