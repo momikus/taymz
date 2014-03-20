49d5d729e0bcdf4e027dc43d7a42b9d68bff30e9
momikus <yavuzkirez@gmail.com>
2014-03-20 16:38:54 +0200

    anasayfada tüm resimler yüklendikten sonra resimler geliyor

--- client/helpers/gallery.js
+++ client/helpers/gallery.js
@@ -2,24 +2,25 @@ Template.gallery.helpers({
 
 	// set the image to be displayed in carousel
 	img: function () {
-
-		if (Timeline.findOne({_id: this._id}) !== undefined) {
-			var milestones = Timeline.findOne({_id: this._id}).milestones;
-
-			// main image is not set so take the first img
-			if (_.findWhere(milestones, {'mainimg': true}) === undefined)
-				return _(_(_(milestones).sortBy(function (milestones) {
-					return milestones.day;
-				})).sortBy(function (milestones) {
-					return milestones.month;
-				})).sortBy(function (milestones) {
-					return milestones.year;
-				})[0].img;
-
-			// main image is set so take that
-			else
-				return _.findWhere(milestones, {'mainimg': true}).img;
-		}
+		
+			if (Timeline.findOne({_id: this._id}) !== undefined) {
+				var milestones = Timeline.findOne({_id: this._id}).milestones;
+	
+				// main image is not set so take the first img
+				if (_.findWhere(milestones, {'mainimg': true}) === undefined)
+					return _(_(_(milestones).sortBy(function (milestones) {
+						return milestones.day;
+					})).sortBy(function (milestones) {
+						return milestones.month;
+					})).sortBy(function (milestones) {
+						return milestones.year;
+					})[0].img;
+	
+				// main image is set so take that
+				else
+					return _.findWhere(milestones, {'mainimg': true}).img;
+			}
+		
 	},
 
 	// olay count alıp galeride göstermek
@@ -39,3 +40,9 @@ Template.gallery.helpers({
 	}
 
 });
+
+Template.gallery.rendered = function () {
+	$(window).load(function() {
+		$('.imageContainer').css('visibility','visible');
+	});
+}
