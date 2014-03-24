02e226c328353746eb722c83d2c3606ece9fa5ae
momikus <yavuzkirez@gmail.com>
2014-03-24 15:32:24 +0200

    temizlik

--- client/helpers/gallery.js
+++ client/helpers/gallery.js
@@ -41,36 +41,9 @@ Template.gallery.helpers({
 });
 
 Template.gallery.rendered = function () {
-$(window).load(function() {
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-});
-
-
-
 	//Bu kod galeride tüm resimler yüklendikten sonra tüm resimlerin aynı anda
 	// gmrünüyor olmasını sağlıyordu ama efektif olmadığı için commentout şimdilik
 	// $(window).load(function() {
 	// 	$('.imageContainer').css('visibility','visible');
-
 	// });
-	//var twitterStr = "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
-	//$('head').append("<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>");
-//$('head').append("");
-
-    // window.___gcfg = {lang: 'tr'};
-    //   (function() {
-    //   var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    //   po.src = 'https://apis.google.com/js/platform.js';
-    //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-    // })();
-
-	// (function(d, s, id) {
-	// 	var js, fjs = d.getElementsByTagName(s)[0];
-	// 	if (d.getElementById(id)) return;
-	// 	js = d.createElement(s); js.id = id;
-	// 	js.src = "//connect.facebook.net/tr_TR/all.js#xfbml=1&appId=1430036597242723";
-	// 	fjs.parentNode.insertBefore(js, fjs);
-	// }(document, 'script', 'facebook-jssdk'));
-
-
 }
--- client/helpers/social.js
+++ client/helpers/social.js
@@ -1,5 +1,6 @@
 Template.social.rendered = function () {
-	Meteor.setTimeout(function() {
+
+		//Twitter için
 		!function(d,s,id){
 			var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
 			if(!d.getElementById(id)){js=d.createElement(s);js.id=id;
@@ -7,6 +8,7 @@ Template.social.rendered = function () {
 			}
 		}(document, 'script', 'twitter-wjs');
 	
+		// g+ için
     	window.___gcfg = {lang: 'tr'};
     	  (function() {
     	  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
@@ -14,6 +16,7 @@ Template.social.rendered = function () {
     	  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
     	})();
 	
+		//fb için
 		(function(d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) return;
@@ -21,5 +24,5 @@ Template.social.rendered = function () {
 			js.src = "//connect.facebook.net/tr_TR/all.js#xfbml=1&appId=1430036597242723";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
-	}, 1500)
+
 }
