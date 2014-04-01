4f86904112f4725194907abd15ab723eb3fa6404
momikus <yavuzkirez@gmail.com>
2014-04-01 21:54:17 +0300

    minor quote fixes

--- client/compatibility/quoter.js
+++ client/compatibility/quoter.js
@@ -1,12 +1,13 @@
 function randomQuote() {
 	var quotes = [
-		"Tarih ihtiyatsızlar için merhametsizdir.<br><strong>Atatürk</strong>", 
+		"Tarih <a href='/t/34'>ihtiyatsız</a>lar için merhametsizdir.<br><strong>Atatürk</strong>", 
 		"Tarihe karşı görevimiz onu yeniden yazmaktır.<br><strong>Oscar Wilde</strong>", 
 		"Bir toplum, iyi tarih yazıyorsa rafine bir toplum olur.<br><strong>İlber Ortaylı</strong>",
 		"Dünya tarihi, özgürlüğün bilincinde ilerlemedir.<br><strong>Georg Wilhelm Friedrich Hegel</strong>",
 		"Filozoflar dünyayı yalnızca çeşitli biçimlerde yorumlamışlardır; oysa sorun onu değiştirmektir.<br><strong>Karl Marx</strong>",
 		"Herkes tarih yapabilir, ancak sadece büyük bir adam tarih yazabilir.<br><strong>Oscar Wilde</strong>",
-		"İki tarih vardır: Yalancı olan resmi tarih, bir de olayların gerçek sebebini barındıran gizli tarih.<br><strong>Honoré de Balzac</strong>",
+		"İki tarih vardır: Yalancı olan resmi tarih, bir de olayların gerçek sebebini barındıran <a href='/t/41'>gizli tarih</a>.<br><strong>Honoré de Balzac</strong>",
+		"Allahıma yemin olsun ki, senin ruhun benim ruhumun önünde diz çöker <a href='/t/67'>tövbe</a> ister!<br><strong>Nihat Doğan - 2007</strong>",
 		"<br><strong></strong>",
 		"<br><strong></strong>",
 		"<br><strong></strong>",
@@ -18,9 +19,10 @@ function randomQuote() {
 		"hegel.jpg",
 		"karl-marx.jpg",
 		"oscar-wilde.jpg",
-		"balzac.jpg"
+		"balzac.jpg",
+		"nihat-dogan.jpg"
 	];
-	var ra = _.random(0, 6)
+	var ra = _.random(0, 7)
 	$('.quote div').html(quotes[ra]);
 	$('#celeb').attr('src','/img/quote/'+images[ra]);
 }
